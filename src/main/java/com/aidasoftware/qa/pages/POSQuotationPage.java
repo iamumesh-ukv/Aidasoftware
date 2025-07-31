ff994d323d8a21e0dc8745b1fa8ada1407334d24
umesh.ukv1392@gmail.com <umesh.ukv131992@gmail.com>
2025-08-01 01:05:59 +0530

    Updated the POSQuotationTest page to navigate on dashboard page and added some attribute xpath

--- src/main/java/com/aidasoftware/qa/pages/POSQuotationPage.java
+++ src/main/java/com/aidasoftware/qa/pages/POSQuotationPage.java
@@ -9,63 +9,59 @@ public class POSQuotationPage {
 
 	WebDriver driver;
 
-	// Object
+	// Objects
 	@FindBy(xpath = "//span[normalize-space()='Add']")
 	WebElement posAddQuoteButton;
-	
-	@FindBy(xpath="//span[@id='select2-ddlCustomerID-container']")
-	WebElement clickOnCustomerDrowpdown;
-	
-	@FindBy(xpath="//ul[@id='select2-ddlCustomerID-results']//li")
-	WebElement selectCustomer;
-	
-	@FindBy(xpath="//ul[@id='select2-ddlCustomerID-results']//li")
-	WebElement clickOnSalesTypeDropdown;
-	
-	@FindBy(xpath="//select[@id='Item1_SalesTypeID']//option")
-	WebElement selectSalesType;
-	
-	@FindBy(xpath="//input[@id='txtInstallationDate']")
-	WebElement clickOnDeliveryDate;
-	
-	@FindBy(xpath="//a[@id='btnAddLineItem']")
-	WebElement clickOnAddItemButton;
-	
-	@FindBy(xpath="//span[@id='select2-ddlItemMaster-container']")
-	WebElement clickOnSelectItemDropdown;
-	
-	@FindBy(xpath="---------------")
-	WebElement selectItem;
-	
-	@FindBy(xpath="//input[@id='txtQty']")
-	WebElement enterQuantity;
-	
-	@FindBy(xpath="//select[@id='ddlWorkTypeID']")
-	WebElement clickOnWorkTypeDropdown;
-	
-	@FindBy(xpath="//select[@id='ddlWorkTypeID']//option")
-	WebElement selectWorkType;
-	
-	@FindBy(xpath="//button[normalize-space()='Save']")
-	WebElement clickOnSaveButton;
-	
-	@FindBy(xpath="//div[@class='mt10']//input[@value='Save']")
-	WebElement clickOnSaveButtonToSaveQuote;
-	
-	@FindBy(xpath="//div[@class='mt10']//input[@value='Save']")
-	WebElement clickOnSubmitAndGenerateSalesOrderButton;
-	
-	
-	
-	
-	
 
+	@FindBy(xpath = "//span[@id='select2-ddlCustomerID-container']")
+	WebElement clickOnCustomerDrowpdown;
+
+	@FindBy(xpath = "//ul[@id='select2-ddlCustomerID-results']//li")
+	WebElement selectCustomer;
+
+	@FindBy(xpath = "//ul[@id='select2-ddlCustomerID-results']//li")
+	WebElement clickOnSalesTypeDropdown;
+
+	@FindBy(xpath = "//select[@id='Item1_SalesTypeID']//option")
+	WebElement selectSalesType;
+
+	@FindBy(xpath = "//input[@id='txtInstallationDate']")
+	WebElement clickOnDeliveryDate;
+
+	@FindBy(xpath = "//a[@id='btnAddLineItem']")
+	WebElement clickOnAddItemButton;
+
+	@FindBy(xpath = "//span[@id='select2-ddlItemMaster-container']")
+	WebElement clickOnSelectItemDropdown;
+
+	@FindBy(xpath = "---------------")
+	WebElement selectItem;
+
+	@FindBy(xpath = "//input[@id='txtQty']")
+	WebElement enterQuantity;
+
+	@FindBy(xpath = "//select[@id='ddlWorkTypeID']")
+	WebElement clickOnWorkTypeDropdown;
+
+	@FindBy(xpath = "//select[@id='ddlWorkTypeID']//option")
+	WebElement selectWorkType;
+
+	@FindBy(xpath = "//button[normalize-space()='Save']")
+	WebElement clickOnSaveButton;
+
+	@FindBy(xpath = "//div[@class='mt10']//input[@value='Save']")
+	WebElement clickOnSaveButtonToSaveQuote;
+
+	@FindBy(xpath = "//div[@class='mt10']//input[@value='Save']")
+	WebElement clickOnSubmitAndGenerateSalesOrderButton;
+
+	//constructor to initialize the object on this page
 	public POSQuotationPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
-	//Actions
+	// Actions
 	public POSQuotationPage clickOnPosQuoteButton() {
 		posAddQuoteButton.click();
 
